c4d03a6158257f2f83dbe9fddc7018773aefa0e7
Hybrid Technologies Solutions <hybrid-technologies-solutions@outlook.com>
2019-08-29 11:03:11 +0200

    fix jquery demo

---
+++ Allfiles/20487C/Mod03/Democode/ConsumingFromHttpClient/Begin/HttpClientApplication.Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace HttpClientApplication.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CallServer();
+            Console.ReadKey();
+        }
+
+        static async Task CallServer()
+        {
+            //Create a new httpClient on http://localhost:12534/
+
+            //Execute GetAsync on 'api/Destinations'
+
+            //Read response content as string
+
+            //Read response content as List of destination
+        }
+    }
+}
